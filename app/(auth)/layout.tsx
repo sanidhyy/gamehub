f710b791c92d9eb92f547749f6a9a5f019a97fa4
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-12 05:00:54 +0000

    logo implemented

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,13 @@
 import { PropsWithChildren } from "react";
 
+import { Logo } from "./_components/logo";
+
 const AuthLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="h-full flex items-center justify-center">{children}</div>
+    <div className="h-full flex flex-col items-center justify-center space-y-6">
+      <Logo />
+      {children}
+    </div>
   );
 };
 
