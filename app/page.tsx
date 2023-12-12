a0c96c1fafaf5ee35a72c734140af72556051368
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-12 03:37:03 +0000

    npx shadcn-ui@latest add button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <p className="text-red-500 font-bold text-3xl">Hello Twitch Clone</p>
+      <Button variant="destructive" size="lg">
+        Get started
+      </Button>
     </div>
   );
 }
