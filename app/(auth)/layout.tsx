9d8af756000fe578317114af0ad0393eff7a4c7e
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-12 04:35:38 +0000

    clerk layout fix

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,9 @@
 import { PropsWithChildren } from "react";
 
 const AuthLayout = ({ children }: PropsWithChildren) => {
-  return <div className="">{children}</div>;
+  return (
+    <div className="h-full flex items-center justify-center">{children}</div>
+  );
 };
 
 export default AuthLayout;
