498ce2acf4c903929521950f51cd0be1bc6dd7e7
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-12 04:34:15 +0000

    clerk sign in and sign up pages created

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import { PropsWithChildren } from "react";
+
+const AuthLayout = ({ children }: PropsWithChildren) => {
+  return <div className="">{children}</div>;
+};
+
+export default AuthLayout;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <p className="text-red-500 font-bold text-3xl">
-        Only authenticated users can see this.
-      </p>
+      <h1 className="text-red-500 font-bold text-3xl">Dashboard</h1>
+      <UserButton afterSignOutUrl="/sign-in" />
     </div>
   );
 }
