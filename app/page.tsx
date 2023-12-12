27d75ff43ab2b100f607eae7af7da05173a2a271
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-12 04:01:33 +0000

    env added to gitignore

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <div>
-      <p className="text-red-500 font-bold text-3xl">Hello Twitch Clone</p>
-      <Button variant="destructive" size="lg">
-        Get started
-      </Button>
+      <p className="text-red-500 font-bold text-3xl">
+        Only authenticated users can see this.
+      </p>
     </div>
   );
 }
