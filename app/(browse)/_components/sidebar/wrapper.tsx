d3159283235df71ba84b8919886138ac3f725772
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-13 09:20:00 +0000

    sidebar wrapper store created

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,8 +1,20 @@
-import { PropsWithChildren } from "react";
+"use client";
+
+import type { PropsWithChildren } from "react";
+
+import { cn } from "@/lib/utils";
+import { useSidebar } from "@/store/use-sidebar";
 
 export const Wrapper = ({ children }: PropsWithChildren) => {
+  const { collapsed } = useSidebar((state) => state);
+
   return (
-    <aside className="fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2d2e35] z-50">
+    <aside
+      className={cn(
+        "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2d2e35] z-50",
+        collapsed && "w-[70px]"
+      )}
+    >
       {children}
     </aside>
   );
