c20f8f547702b339811b6ba30c7ce3a84c2e5080
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-13 16:48:20 +0000

    stream service toggler created

---
+++ app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { toast } from "sonner";
+import { useTransition } from "react";
+
+import { updateStream } from "@/actions/stream";
+import { Switch } from "@/components/ui/switch";
+
+type FieldTypes = "isChatEnabled" | "isChatDelayed" | "isChatFollowersOnly";
+
+type ToggleCardProps = {
+  label: string;
+  value: boolean;
+  field: FieldTypes;
+};
+
+export const ToggleCard = ({
+  label,
+  value = false,
+  field,
+}: ToggleCardProps) => {
+  const [isPending, startTransition] = useTransition();
+
+  const onChange = () => {
+    startTransition(() => {
+      updateStream({ [field]: !value })
+        .then(() => toast.success("Chat settings updated."))
+        .catch(() => toast.error("Something went wrong."));
+    });
+  };
+
+  return (
+    <div className="rounded-xl bg-muted p-6">
+      <div className="flex items-center justify-between">
+        <p className="font-semibold shrink-0">{label}</p>
+        <div className="space-y-2">
+          <Switch
+            onCheckedChange={onChange}
+            disabled={isPending}
+            checked={value}
+          >
+            {value ? "On" : "Off"}
+          </Switch>
+        </div>
+      </div>
+    </div>
+  );
+};
