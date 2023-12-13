808e0cb25a508c3895359ce3754bc61bfce349fd
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-13 16:48:09 +0000

    stream settings action created

---
+++ actions/stream.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import type { Stream } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+
+import { db } from "@/lib/db";
+import { getSelf } from "@/lib/auth-service";
+
+export const updateStream = async (values: Partial<Stream>) => {
+  try {
+    const self = await getSelf();
+    const selfStream = await db.stream.findUnique({
+      where: {
+        userId: self.id,
+      },
+    });
+
+    if (!selfStream) throw new Error("Stream not found.");
+
+    const validData = {
+      name: values.name,
+      isChatEnabled: values.isChatEnabled,
+      isChatFollowersOnly: values.isChatFollowersOnly,
+      isChatDelayed: values.isChatDelayed,
+    };
+
+    const stream = await db.stream.update({
+      where: {
+        id: selfStream.id,
+      },
+      data: {
+        ...validData,
+      },
+    });
+
+    revalidatePath(`/u/${self.username}/chat`);
+    revalidatePath(`/u/${self.username}`);
+    revalidatePath(`/${self.username}`);
+
+    return stream;
+  } catch (error: unknown) {
+    throw new Error("Internal Server Error");
+  }
+};
