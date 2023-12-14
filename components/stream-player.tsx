478c9eff3b2ecebaf2406c90c0488befb16f1aaa
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-14 06:20:37 +0000

    viewer token setup

---
+++ components/stream-player.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useViewerToken } from "@/hooks/use-viewer-token";
+import type { Stream, User } from "@prisma/client";
+
+type StreamPlayerProps = {
+  user: User & {
+    stream: Stream | null;
+  };
+  stream: Stream;
+  isFollowing: boolean;
+};
+
+export const StreamPlayer = ({
+  user,
+  stream,
+  isFollowing,
+}: StreamPlayerProps) => {
+  const { token, name, identity } = useViewerToken(user.id);
+
+  if (!token || !name || !identity) {
+    return <div className="">Cannot watch the stream.</div>;
+  }
+
+  return <div className="">Allowed to watch the stream.</div>;
+};
