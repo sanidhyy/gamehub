2266e6fe40ccb3db300713f10d6e137481b4c3a6
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-14 07:28:53 +0000

    offline video implemented

---
+++ components/stream-player/video.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { ConnectionState, Track } from "livekit-client";
+import {
+  useConnectionState,
+  useRemoteParticipant,
+  useTracks,
+} from "@livekit/components-react";
+
+import { OfflineVideo } from "./offline-video";
+import { LoadingVideo } from "./loading-video";
+
+type VideoProps = {
+  hostName: string;
+  hostIdentity: string;
+};
+
+export const Video = ({ hostName, hostIdentity }: VideoProps) => {
+  const connectionState = useConnectionState();
+  const participant = useRemoteParticipant(hostIdentity);
+  const tracks = useTracks([
+    Track.Source.Camera,
+    Track.Source.Microphone,
+  ]).filter((track) => track.participant.identity === hostIdentity);
+
+  let content;
+
+  if (!participant && connectionState === ConnectionState.Connected)
+    content = <OfflineVideo username={hostName} />;
+  else if (!participant || tracks.length === 0)
+    content = <LoadingVideo label={connectionState} />;
+  else content = <p>Live video</p>;
+
+  return <div className="aspect-video border-b group relative">{content}</div>;
+};
