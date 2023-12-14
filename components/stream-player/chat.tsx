78199856974e50510530314b3c97f28ee0f0e52a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-14 08:51:38 +0000

    chat toggler implemented

---
+++ components/stream-player/chat.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useEffect, useMemo, useState } from "react";
+import {
+  useChat,
+  useConnectionState,
+  useRemoteParticipant,
+} from "@livekit/components-react";
+import { ConnectionState } from "livekit-client";
+import { useMediaQuery } from "usehooks-ts";
+
+import { useChatSidebar } from "@/store/use-chat-sidebar";
+
+import { ChatHeader } from "./chat-header";
+
+type ChatProps = {
+  hostName: string;
+  hostIdentity: string;
+  viewerName: string;
+  isFollowing: boolean;
+  isChatEnabled: boolean;
+  isChatDelayed: boolean;
+  isChatFollowersOnly: boolean;
+};
+
+export const Chat = ({
+  hostName,
+  hostIdentity,
+  viewerName,
+  isFollowing,
+  isChatEnabled,
+  isChatDelayed,
+  isChatFollowersOnly,
+}: ChatProps) => {
+  const matches = useMediaQuery("(max-width: 1024px)");
+  const { variant, onExpand } = useChatSidebar();
+  const connectionState = useConnectionState();
+  const participant = useRemoteParticipant(hostIdentity);
+
+  const isOnline = participant && connectionState === ConnectionState.Connected;
+  const isHidden = !isChatEnabled || !isOnline;
+
+  const [value, setValue] = useState("");
+  const { chatMessages: messages, send } = useChat();
+
+  useEffect(() => {
+    if (matches) onExpand();
+  }, [matches, onExpand]);
+
+  const reversedMessages = useMemo(() => {
+    return messages.sort((a, b) => b.timestamp - a.timestamp);
+  }, [messages]);
+
+  const onSubmit = () => {
+    if (!send) return;
+
+    send(value);
+    setValue("");
+  };
+
+  const onChange = (value: string) => setValue(value);
+
+  return (
+    <div className="flex flex-col bg-background border-l border-b pt-0 h-[calc(100vh-80px)]">
+      <ChatHeader />
+    </div>
+  );
+};
--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -3,8 +3,13 @@
 import type { Stream, User } from "@prisma/client";
 import { LiveKitRoom } from "@livekit/components-react";
 
+import { useChatSidebar } from "@/store/use-chat-sidebar";
 import { useViewerToken } from "@/hooks/use-viewer-token";
 import { Video } from "@/components/stream-player/video";
+import { cn } from "@/lib/utils";
+
+import { ChatToggle } from "./chat-toggle";
+import { Chat } from "./chat";
 
 type StreamPlayerProps = {
   user: User & {
@@ -20,6 +25,7 @@ export const StreamPlayer = ({
   isFollowing,
 }: StreamPlayerProps) => {
   const { token, name, identity } = useViewerToken(user.id);
+  const { collapsed } = useChatSidebar((state) => state);
 
   if (!token || !name || !identity) {
     return <div className="">Cannot watch the stream.</div>;
@@ -27,13 +33,33 @@ export const StreamPlayer = ({
 
   return (
     <>
+      {collapsed && (
+        <div className="hidden lg:block fixed top-[100px] right-2 z-50">
+          <ChatToggle />
+        </div>
+      )}
       <LiveKitRoom
         token={token}
         serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
-        className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
+        className={cn(
+          "grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full",
+          collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2"
+        )}
       >
         <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
           <Video hostName={user.username} hostIdentity={user.id} />
+        </div>
+
+        <div className={cn("col-span-1", collapsed && "hidden")}>
+          <Chat
+            viewerName={name}
+            hostName={user.username}
+            hostIdentity={user.id}
+            isFollowing={isFollowing}
+            isChatEnabled={stream.isChatEnabled}
+            isChatDelayed={stream.isChatDelayed}
+            isChatFollowersOnly={stream.isChatFollowersOnly}
+          />
         </div>
       </LiveKitRoom>
     </>
