9a65d445e1d0b808d2aa57e39a908a63ee16898a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-14 08:07:07 +0000

    full screen control implemented

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { type Participant, Track } from "livekit-client";
 import { useTracks } from "@livekit/components-react";
+import { useEventListener } from "usehooks-ts";
+
+import FullscreenControl from "./fullscreen-control";
 
 type LiveVideoProps = {
   participant: Participant;
@@ -11,6 +14,20 @@ type LiveVideoProps = {
 export const LiveVideo = ({ participant }: LiveVideoProps) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
+
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  const toggleFullscreen = () => {
+    if (isFullscreen) document.exitFullscreen();
+    else if (wrapperRef.current) wrapperRef.current?.requestFullscreen();
+  };
+
+  const handleFullscreenChange = () => {
+    const isCurrentlyFullscreen = document.fullscreenElement !== null;
+    setIsFullscreen(isCurrentlyFullscreen);
+  };
+
+  useEventListener("fullscreenchange", handleFullscreenChange, wrapperRef);
 
   useTracks([Track.Source.Camera, Track.Source.Microphone])
     .filter((track) => track.participant.identity === participant.identity)
@@ -23,6 +40,15 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
   return (
     <div ref={wrapperRef} className="relative h-full flex">
       <video ref={videoRef} width="100%" />
+
+      <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 transition-all">
+        <div className="absolute bottom-0 flex h-14 w-full items-center justify-between bg-gradient-to-r from-neutral-900 px-4">
+          <FullscreenControl
+            isFullscreen={isFullscreen}
+            onToggle={toggleFullscreen}
+          />
+        </div>
+      </div>
     </div>
   );
 };
