f09872c4ec882b9bd6a707e626cac4da120c406f
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-14 09:44:44 +0000

    fix stringto color util

--- lib/utils.ts
+++ lib/utils.ts
@@ -16,7 +16,7 @@ export const stringToColor = (str: string) => {
 
   for (let i = 0; i < 3; i++) {
     const value = (hash >> (i * 8)) & 0xff;
-    color += ("00" + value.toString(16)).substring(-2);
+    color += ("00" + value.toString(16)).substr(-2);
   }
 
   return color;
