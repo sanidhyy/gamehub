2b6eccee4f7d17aacba77737bad1968ba6014c8c
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-15 06:27:21 +0000

    stream player added to viewer page

--- actions/block.ts
+++ actions/block.ts
@@ -1,16 +1,35 @@
 "use server";
 
-import { blockUser, unblockUser } from "@/lib/block-service";
+import { RoomServiceClient } from "livekit-server-sdk";
 import { revalidatePath } from "next/cache";
 
+import { blockUser, unblockUser } from "@/lib/block-service";
+import { getSelf } from "@/lib/auth-service";
+
+const roomService = new RoomServiceClient(
+  process.env.LIVEKIT_API_URL!,
+  process.env.LIVEKIT_API_KEY!,
+  process.env.LIVEKIT_API_SECRET!
+);
+
 export const onBlock = async (id: string) => {
-  // TODO: Adapt to disconnect from livestream
-  // TODO: Allow ability to kick the guest
-  const blockedUser = await blockUser(id);
+  const self = await getSelf();
 
-  revalidatePath("/");
+  let blockedUser;
 
-  if (blockedUser) revalidatePath(`/${blockedUser.blocked.username}`);
+  try {
+    blockedUser = await blockUser(id);
+  } catch (error: unknown) {
+    // User is a guest
+  }
+
+  try {
+    await roomService.removeParticipant(self.id, id);
+  } catch (error: unknown) {
+    // user is not in the room
+  }
+
+  revalidatePath(`/u/${self.username}/community`);
 
   return blockedUser;
 };
--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,9 +1,9 @@
 import { notFound } from "next/navigation";
 
-import { Actions } from "./_components/actions";
 import { getUserByUsername } from "@/lib/user-service";
 import { isFollowingUser } from "@/lib/follow-service";
 import { isBlockedByUser } from "@/lib/block-service";
+import { StreamPlayer } from "@/components/stream-player";
 
 type UserPageProps = {
   params: {
@@ -14,18 +14,20 @@ type UserPageProps = {
 const UserPage = async ({ params }: UserPageProps) => {
   const user = await getUserByUsername(params.username);
 
-  if (!user) notFound();
+  if (!user || !user.stream) notFound();
 
   const isFollowing = await isFollowingUser(user.id);
   const isBlocked = await isBlockedByUser(user.id);
 
+  if (isBlocked) notFound();
+
   return (
-    <div className="flex flex-col gap-y-4">
-      <p>Username: {params.username}</p>
-      <p>User ID: {user.id}</p>
-      <p>Following: {isFollowing ? "Yes" : "No"}</p>
-      <p>Blocked: {isBlocked ? "Yes" : "No"}</p>
-      <Actions userId={user.id} isFollowing={isFollowing} />
+    <div className="">
+      <StreamPlayer
+        user={user}
+        stream={user.stream}
+        isFollowing={isFollowing}
+      />
     </div>
   );
 };
