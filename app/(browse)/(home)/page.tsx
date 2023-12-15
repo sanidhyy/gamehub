875f2256c4e46467f3e45cf95206aa1200d272b7
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-15 07:22:34 +0000

    results component implemented & added to home

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,7 +1,15 @@
+import { Suspense } from "react";
+
+import { Results, ResultsSkeleton } from "./_components/results";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="font-bold text-3xl">Home Page</h1>
+      <h1 className="h-full p-8 max-w-screen-2xl mx-auto">
+        <Suspense fallback={<ResultsSkeleton />}>
+          <Results />
+        </Suspense>
+      </h1>
     </div>
   );
 }
